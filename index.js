caef23c34c062b178d96a0f6bc9949e0a38b447f
Gabriel Imre <gabrielimre@work.local>
2016-12-06 13:59:40 +0200

    added solution from cristipavel

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict'
 
-exports = module.exports = n => Math.pow( 10, n ) - Math.pow( 9, n )
+module.exports = function number(n, d) {
+    var i = 0;
+	var count = 0;
+    var m = 10^n;
+	
+	for (i=1; i<m; i++){
+		while (i>0) {
+			if (i%10 == d) {
+				count++;
+				break;
+			}else{
+				i = i/10;
+			}
+		}
+	}
+    return count;
+};
