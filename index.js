b68a75d7391310dbd97c7c95849759786b12c583
Gabriel Imre <gabrielimre@work.local>
2016-12-06 14:56:32 +0200

    added solution from Ale282313

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 'use strict'
 
-module.exports = function number(n, d) {
-    var i = 0;
-	var count = 0;
-    var m = 10^n;
-	
-	for (i=1; i<m; i++){
-		while (i>0) {
-			if (i%10 == d) {
-				count++;
-				break;
-			}else{
-				i = i/10;
-			}
-		}
-	}
-    return count;
+exports = function(n,d){
+  if(d==0){
+    var total = count0(n);
+    return total;
+
+  }else{
+    var total=count(n);
+    return total;
+  }
+
 };
+
+function countUpTo(d){
+  var first = 9*((Math.pow(10,d)-1)/9);
+  var second= 9*((Math.pow(9,d)-1)/8);
+  return first-second;
+}
+function count0(n){
+  var length = Math.log(Math.pow(10,n)) * Math.LOG10E + 1 | 0;
+  var total1 = countUpTo(length-1);
+
+  return total1+1;
+}
+
+  // Tn=9Tn-1+10^(n-1)
+function count(n){
+  if(n==1){
+    return 1;
+  }else{
+      return Math.pow(10, n-1)+9*count(n-1);
+  }
+}
